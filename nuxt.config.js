a88522d49ff156abd5555824b438121d745bb4d2
John Bailey <641367+johntimothybailey@users.noreply.github.com>
2018-04-09 11:03:35 -1000

    Adding Webpack loader for SCSS/Sass and including Atoms as a default

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,13 +10,21 @@ module.exports = {
       { hid: 'description', name: 'description', content: 'Sam Mason Music' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' }
     ]
   },
   /*
   ** Customize the progress bar color
   */
   loading: { color: '#3B8070' },
+
+  /**
+   * CSS
+   */
+  css: [
+    '@/assets/atoms.scss'
+  ],
 
   plugins: [
   ],
