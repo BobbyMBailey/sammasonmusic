9d677bd6e6a4039ec94878df5dd7e82ac9029707
BobbyMBailey <RobertBailey7@gmail.com>
2018-04-11 18:56:34 -0500

    card components

---
+++ components/Cards.vue
@@ -0,0 +1,17 @@
+<template>
+  <div :class="bem()">
+    <div :class="bem('media')"/>
+    <div :class="bem('title')"/>
+    <div :class="bem('text')"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
