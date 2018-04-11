e050c9771c37ea4fc300a7edb4c5c0cc4503e57b
BobbyMBailey <RobertBailey7@gmail.com>
2018-04-10 22:29:20 -0500

    Adding typography, colors (atoms)

--- pages/v2/styleguide/index.vue
+++ pages/v2/styleguide/index.vue
@@ -1,5 +1,35 @@
 <template>
   <article>
+    <mason-section title="Typography">
+      <div>
+        <p>Hello I am a paragraph</p>
+      </div>
+      <div>
+        <h1>I am a header 1</h1>
+      </div>
+      <div>
+        <h2>I am a header 2</h2>
+      </div>
+      <div>
+        <h3>I am a header 3</h3>
+      </div>
+      <div>
+        <h4>I am a header 4</h4>
+      </div>
+      <div>
+        <h5>I am a header 5</h5>
+      </div>
+      <div>
+        <h6>I am a header 6</h6>
+      </div>
+    </mason-section>
+    <mason-section title="Neutral Colors">
+      <div class="mason-swatch atom-background-color--black"/>
+      <div class="mason-swatch atom-background-color--darkteal"/>
+      <div class="mason-swatch atom-background-color--lightteal"/>
+      <div class="mason-swatch atom-background-color--lighterteal"/>
+      <div class="mason-swatch atom-background-color--gray"/>
+    </mason-section>
     <mason-section
       title="Footer">
       <mason-footer/>
@@ -21,4 +51,10 @@ export default {
 </script>
 
 <style scoped>
+.mason-swatch {
+  border-radius: 4px;
+  width: 1em;
+  height: 1em;
+  border: 4px solid transparent;
+}
 </style>
