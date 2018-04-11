45f3b5bc0ead0d774a3fd49208db8dd3a162bf0e
John Bailey <641367+johntimothybailey@users.noreply.github.com>
2018-04-11 11:20:57 -1000

    #23 No Robots

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,9 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Sam Mason Music' }
+      { hid: 'description', name: 'description', content: 'Sam Mason Music' },
+      /** Eventually this will move just to the StyleGuide layout as https://github.com/nuxt/nuxt.js/issues/3036 */
+      { hid: 'robots', name: 'robots', content: 'noindex'}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
