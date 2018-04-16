3e6d9035c1c3ef814d575b21f17f26c29480cb9d
John Bailey <641367+johntimothybailey@users.noreply.github.com>
2018-04-15 22:20:42 -1000

    User MDC Button with the previous styles remaining

--- components/Button.vue
+++ components/Button.vue
@@ -28,8 +28,10 @@ export default {
 
 <style lang="scss" scoped>
   @import 'assets/theme/colors';
+  @import '@material/button/mdc-button';
+  @import '@material/button/mixins';
 
-  .mason-button {
+  .mason-button-other {
     align-items: center;
     border: 1px solid $mason-theme-color--primary;
     border-radius: 2px;
@@ -45,7 +47,7 @@ export default {
     text-transform: uppercase;
   }
 
-  .mason-button:hover {
+  .mason-button-other:hover {
     position: relative;
     top: 1px;
     background-color: $theme-background-color--tertiary-dark;
@@ -53,8 +55,42 @@ export default {
     opacity: 0.7;
   }
 
-  a {
-    color: $typography-color--primary;
+  .mason-button {
+    @extend .mdc-button;
+    @extend .mdc-button--stroked;
+    @include mdc-button-stroke-width(1px);
+  }
+
+  // Alternate way and retaining the modifiers. However, for Sam we're probably wanting the default to be "stroked"
+  .mason-button--raised,
+  .mason-button--unelevated,
+  .mason-button--stroked {
+    .mason-button__icon {
+      // Icons inside contained buttons have different styles due to increased button padding
+      @include mdc-button__icon-contained_;
+    }
+  }
+
+  .mason-button--raised,
+  .mason-button--unelevated {
+    @include mdc-button--filled_;
+    @include mdc-button-container-fill-color(primary);
+    @include mdc-button-ink-color(text-primary-on-primary);
+    @include mdc-states(text-primary-on-primary);
+  }
+
+  .mason-button--raised {
+    @include mdc-button--raised_;
+  }
+
+  .mason-button--stroked {
+    @include mdc-button--stroked_;
+    @include mdc-button-stroke-width(1px);
+    @include mdc-button-stroke-color(primary);
+  }
+
+  .mason-button--dense {
+    @include mdc-button--dense_;
   }
 
 </style>
