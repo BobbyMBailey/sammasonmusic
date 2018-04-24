0e3b35a2728fff18e7834a4924905f496e15b810
John Bailey <641367+johntimothybailey@users.noreply.github.com>
2018-04-24 10:01:54 -1000

    Introducing Signup

---
+++ components/Signup.vue
@@ -0,0 +1,23 @@
+<template>
+  <div :class="bem()">
+    <mason-email-text-field/>
+    <mason-button label="Submit"/>
+  </div>
+</template>
+
+<script>
+import MasonButton from './Button.vue'
+import MasonEmailTextField from './EmailTextField.vue'
+
+export default {
+  name: 'Signup',
+  components: {
+    MasonButton,
+    MasonEmailTextField
+  }
+}
+</script>
+
+<style lang="scss">
+  /** No style */
+</style>
---
+++ tests/unit/components/Signup.spec.js
@@ -0,0 +1,57 @@
+import { expect } from 'chai'
+import { createLocalVue, shallow } from '@vue/test-utils'
+import ComponentUnderTest from '../../../components/Signup.vue'
+import setupPlugins from './_setup/plugins'
+
+describe('Signup', () => {
+  let localVue
+  const blockNamespace = 'mason'
+  const componentName = 'signup'
+  let stubs = {
+    'mason-button': '<a/>',
+    'mason-email-text-field': '<input/>'
+  }
+
+  before(() => {
+    localVue = createLocalVue()
+    setupPlugins(localVue)
+  })
+
+  describe('Accessibility', () => {
+    it('provides anchor tag and input field', () => {
+      const wrapper = shallow(ComponentUnderTest, {
+        localVue,
+        propsData: {
+          items: [
+            { label: '-' },
+            { label: '-' }
+          ]
+        },
+        stubs: stubs
+      })
+
+      expect(wrapper.contains('a')).equal(true)
+      expect(wrapper.contains('input')).equal(true)
+    })
+  })
+
+  describe('BEM', () => {
+    it(`using namespace of ${blockNamespace}`, () => {
+      const wrapper = shallow(ComponentUnderTest, {
+        localVue,
+        propsData: {
+          items: [
+            { label: '-', icon: [] },
+            { label: '-', icon: [] }
+          ]
+        },
+        stubs: stubs
+      })
+      expect(wrapper.contains(`.${blockNamespace}-${componentName}`)).equal(true, 'contains Block')
+      expect(wrapper.contains(`.${blockNamespace}-${componentName}__submit`)).equal(true, 'contains submit button Element')
+      expect(wrapper.contains(`.${blockNamespace}-${componentName}__email`)).equal(true, 'contains email text field Element')
+    })
+  })
+
+  describe.skip('Properties', () => {})
+})
