b85a5578c545ec439ec78160ea1901fc9fd5b231
John Bailey <641367+johntimothybailey@users.noreply.github.com>
2018-05-02 15:15:00 -1000

    Press Content updated

--- pages/v2/press.vue
+++ pages/v2/press.vue
@@ -7,6 +7,24 @@
       background-image="/v2/images/4749.jpg"/>
     <mason-section :block-modifiers="['center']">
       <mason-card class="mason-card--quote mason-card--full">
+        <p slot="text">When I first heard Sam's music, I knew I was dealing with a deep thinker and an authentic artist. And now that I know him -- and his fantastic music -- a lot better, I know my first instincts were right. He writes truthful, poetic, soul-baring songs that deserve to be heard.</p>
+        <p slot="cite">Ross King</p>
+      </mason-card>
+
+      <mason-card class="mason-card--quote mason-card--full">
+        <p slot="text">Sam Mason’s music bleeds. It is raw, honest, and gritty. Haunting melodies and gut-wrenching lyrics confront the listener and demand a response. Humanity, however light or dark it can be, drives Mason to reflect it’s essence in his songs.</p>
+        <p slot="cite">Randy Fuller</p>
+        <p
+          slot="cite"
+          class="mason-typography--caption">The Village Church – Worship Leader</p>
+      </mason-card>
+
+      <mason-card class="mason-card--quote mason-card--full">
+        <p slot="text">Mazzy Star meets Mojave 3! Could listen to it for hours.</p>
+        <p slot="cite">Taylor Tatsch</p>
+      </mason-card>
+
+      <mason-card class="mason-card--quote mason-card--full">
         <p slot="text">Mason's laid-back vocals are perfectly matched to Tatsch's spacious, lap steel-laced soundscapes and his evocative lyrics, particularly on the title track, <i>Give Me a Sign</i>, suggest sideman Mason would fare just fine if he decided to make himself the focus full-time.</p>
         <p slot="cite">Preston Jones</p>
         <a
